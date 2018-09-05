6e5591965b3e138a94e1922175e44ef5281df6f7
lanbing <liyong@nenu.edu.cn>
2018-09-05 15:32:20 +0800

    缓存模块,封装Memcache,Memcached,Redis,File,None 多种缓存方式

---
+++ src/functions.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace icePHP;
+
+/**
+ * 获取一个缓存实例
+ * 这是一个SCache的快捷入口
+ * @param string $type Page/Data: 页面缓存/数据缓存
+ * @return CacheBase
+ */
+function cache(string $type): CacheBase
+{
+    return CacheFactory::instance($type);
+}
